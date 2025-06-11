83b4eba6be00477b150c899a95c94822505e0ed4
dainisstrumpe <danisstrumpe@startschool.org>
2025-06-11 18:06:37 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from './components/calculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Calculator</h1>
+      <Calculator />
+    </div>
+  )
+};
+
+export default App;
